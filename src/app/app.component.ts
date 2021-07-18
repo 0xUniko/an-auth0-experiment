3a8f0f3e8ad97ec31f593ebfaeb4cedd9ea0b17c
ls <iwonttellu@anonymous.com>
2021-07-18 21:00:36 +0800

    the second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'a-simple-login-example';
+  title = 'My App';
+
+  header = 'header';
+
+  constructor(public auth: AuthService, private http: HttpClient) {}
+
+  loginWithRedirect() {
+    this.auth.loginWithRedirect();
+    // this.auth.loginWithRedirect({ screen_hint: 'signup' });
+  }
+
+  logout() {
+    this.auth.logout({ returnTo: window.location.origin });
+  }
+
+  fetchApi() {
+    this.http
+      .get<{ header: string }>('api/HttpTrigger1')
+      .subscribe((response) => {
+        this.header = response.header;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule.forRoot({
+      domain: 'dev-3rae35pa.jp.auth0.com',
+      clientId: 'EGiW0SOiomkRtHrzvEjAPQ6oOrkMkXzj',
+      audience: 'api-for-azure-functions',
+      httpInterceptor: {
+        allowedList: [{ uri: 'api/*', allowAnonymous: true }],
+        // allowedList: [{ uri: 'api/HttpTrigger1' }],
+      },
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
